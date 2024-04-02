d18aa290ab81112e708ccd5ce96156dbbc4c21e3
Jvitor1303 <jvitor.estevam18@gmail.com>
2024-04-02 11:40:50 -0300

    eu peguei as variaveis 'nome' e 'categoria' da classe restaurante e atribui um nome e uma categoria, e depois usei o 'vars' no print para exibir a variavel restaurante_pizza

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -4,9 +4,14 @@ class Restaurante:
     ativo = False 
 
 restaurante_pizza = Restaurante() 
+restaurante_pizza.nome = 'Pizzaria do Papaléguas'
+restaurante_pizza.categoria = 'Pizzaria' 
+
+
+
 restaurante_macarrao = Restaurante() 
 
 restaurantes = [restaurante_macarrao, restaurante_pizza] 
 
-print(restaurantes)
+print(vars(restaurante_pizza))
 
