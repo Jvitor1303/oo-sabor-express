3adaf806184500fc787648ec8acaf244e0ede4dc
Jvitor1303 <jvitor.estevam18@gmail.com>
2024-04-02 11:18:02 -0300

    primeiro commit

---
+++ modelos/restaurante.py
@@ -0,0 +1,12 @@
+class Restaurante: 
+    nome = ''
+    categoria = '' 
+    atvo = False 
+
+restaurante_pizza = Restaurante() 
+restaurante_macarrao = Restaurante() 
+
+restaurantes = [restaurante_macarrao, restaurante_pizza] 
+
+print(restaurantes)
+
