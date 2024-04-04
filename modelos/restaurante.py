ec4a5c88077653cde2dc7b45bc31b1dc146c3b36
Jvitor1303 <jvitor.estevam18@gmail.com>
2024-04-04 13:04:41 -0300

    commit

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,14 +1,15 @@
 class Restaurante: 
-    def __init__():
-        nome = ''
-        categoria = '' 
-        ativo = False 
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria 
+        self.ativo = False 
 
-restaurante_pizza = Restaurante() 
+restaurante_pizza = Restaurante('Pizzaria do Papaléguas', 'Italiana') 
  
-restaurante_macarrao = Restaurante() 
+restaurante_macarrao = Restaurante('Macarronada da Dna. Joana', 'Massas em geral') 
 
 restaurantes = [restaurante_macarrao, restaurante_pizza] 
 
 print(vars(restaurante_pizza))
+print(vars(restaurante_macarrao))
 
