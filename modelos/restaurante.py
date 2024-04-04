e1a9319114d39dbefa996c500ba4c793752f3fdd
Jvitor1303 <jvitor.estevam18@gmail.com>
2024-04-04 15:25:05 -0300

    foi adicionado o método especial __srt__ para garantir que o objeto(classe)  seja lida como string e não realmete como um objeto

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,7 +2,10 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria 
-        self.ativo = False 
+        self.ativo = False  
+    
+    def __str__(self): 
+        return f'{self.nome} | {self.categoria}'   
 
 restaurante_pizza = Restaurante('Pizzaria do Papaléguas', 'Italiana') 
  
@@ -10,6 +13,8 @@ restaurante_macarrao = Restaurante('Macarronada da Dna. Joana', 'Massas em geral
 
 restaurantes = [restaurante_macarrao, restaurante_pizza] 
 
-print(vars(restaurante_pizza))
-print(vars(restaurante_macarrao))
-
+print()
+print(restaurante_pizza) 
+print()
+print(restaurante_macarrao)
+print()
