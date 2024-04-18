09a408147283e6805b5342fd9af1dc832abe4909
Jvitor1303 <jvitor.estevam18@gmail.com>
2024-04-18 15:52:06 -0300

    coloquei uma função para alternar o estado do restaurante

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -20,10 +20,15 @@ class Restaurante:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}') 
     @property 
     def ativo(self): 
-        return '✅' if self._ativo else '❌ '
+        return '✅' if self._ativo else '❌ ' 
+    
+    def alternar_estado(self): 
+        self._ativo = not self._ativo
+
+
 #instância é a 'variável' que atribuimos á classe. Essas duas variáveis são instâncias da classe Restaurante
 restaurante_pizza = Restaurante('pizzaria do papaléguas', 'italiana') 
-
+restaurante_pizza.alternar_estado()
 restaurante_macarrao = Restaurante('Macarronada da Dna. Joana', 'Massas em geral') 
 
 Restaurante.listar_restaurantes()
