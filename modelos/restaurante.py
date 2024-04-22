fead77b91a5b1641b4dcd59da47ceda415560f82
Jvitor1303 <jvitor.estevam18@gmail.com>
2024-04-22 13:45:34 -0300

    coloquei a media das avaliaçoes para que aparecesse no meu terminal

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -18,10 +18,10 @@ class Restaurante:
     @classmethod
     def listar_restaurantes(cls):  
         print()
-        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}') 
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}') 
         print()
         for restaurante in cls.restaurantes: 
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}') 
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante._media_avaliacoes).ljust(25)} | {restaurante.ativo}') 
     @property 
     def ativo(self): 
         return '✅' if self._ativo else '❌ ' 
