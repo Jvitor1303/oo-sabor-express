b736a677cde9eb7eacab383233c52d22b6bf68a2
Jvitor1303 <jvitor.estevam18@gmail.com>
2024-04-22 12:12:32 -0300

    adicionei instâncias

--- modelos/app.py
+++ modelos/app.py
@@ -1,10 +1,16 @@
 from modelos.restaurante import Restaurante 
 
+#instância é a 'variável' que atribuimos á classe. Essas duas variáveis são instâncias da classe Restaurante
+restaurante_pizza = Restaurante('pizzaria do papaléguas', 'italiana') 
 
+restaurante_macarrao = Restaurante('Macarronada da Dna. Joana', 'Massas em geral')  
+
+restaurante_mexicano = Restaurante('Mexican food', 'Mexicana')
+ 
 
 
 def main(): 
-    pass
+    Restaurante.listar_restaurantes()
 
 
 if __name__ == '__main__': 
