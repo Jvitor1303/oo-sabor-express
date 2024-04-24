012e63e212464f42103361d687f51e40926c820a
Jvitor1303 <jvitor.estevam18@gmail.com>
2024-04-24 11:44:30 -0300

    importei as classes para o app.py e instanciei essas classe. E no arquivo de cada classe eu coquei para que retornasse em string somente o nome de cada instância

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from modelos.restaurante import Restaurante
+from modelos.restaurante import Restaurante 
+from modelos.cardapio.bebida import Bebida 
+from modelos.cardapio.prato import Prato 
+
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('João', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Carlos', 2)
+
+bebida_suco = Bebida('Suco de Melância', 5.0, 'grande') 
+
+prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade')
 
 def main():
-    Restaurante.listar_restaurantes()
+   print(bebida_suco)
+   print(prato_paozinho)
 
 if __name__ == '__main__':
     main()
