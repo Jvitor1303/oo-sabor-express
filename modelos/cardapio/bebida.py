6ea5a627251069172711dcb55884e6be2367a4e6
Jvitor1303 <jvitor.estevam18@gmail.com>
2024-04-24 11:35:27 -0300

    atribui a classe filho (Bebida) os parâmentros da classe Pai (ItemCardapio)

--- modelos/cardapio/bebida.py
+++ modelos/cardapio/bebida.py
@@ -1,3 +1,6 @@
+from modelos.cardapio.item_cardapio import ItemCardapio
+
 class Bebida: 
     def __init__(self,nome,preco,tamanho): 
-        pass
+        super().__init__(nome,preco)
+        self._tamanho = tamanho
