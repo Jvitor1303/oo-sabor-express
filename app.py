b45f50e0955c0f10500638201a658b792ad18cf5
Jvitor1303 <jvitor.estevam18@gmail.com>
2024-04-25 09:46:02 -0300

    coloquei a bebida_suco e o prato_paozinho dentro das funções anteriormente criadas

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ restaurante_praca = Restaurante('praça', 'Gourmet')
 
 bebida_suco = Bebida('Suco de Melância', 5.0, 'grande') 
 
-prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade')
+prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade') 
+
+restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco) 
+
+restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
 
 def main():
    print(bebida_suco)
