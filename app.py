77c2292b8e1f78b6bf0b23d862c26b64f67e727d
Jvitor1303 <jvitor.estevam18@gmail.com>
2024-04-29 11:40:56 -0300

    otimizei o código para que os item não sejam mais diferentes, então fiz uma função que se a instanância for da classe de parâmetro ela automáticamente vai aceitar como um item do cardápio

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ restaurante_praca = Restaurante('praça', 'Gourmet')
 
 bebida_suco = Bebida('Suco de Melância', 5.0, 'grande') 
 
-prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade') 
+prato_paozinho = Prato('Pãozinho', 2.0, 'O melhor pão da cidade') 
 
-restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco) 
+restaurante_praca.adicionar_no_cardapio(bebida_suco) 
 
-restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
    print(bebida_suco)
    print(prato_paozinho)
 
 if __name__ == '__main__':
-    main()
+   main() 
